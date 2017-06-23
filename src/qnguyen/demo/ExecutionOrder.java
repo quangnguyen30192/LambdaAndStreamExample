ef14bd582c699ad7ee0b5e814a200c7f7f911447
nguyenquang <quangnbnse90114@gmail.com>
2017-06-23 09:32:15 +0700

    commit execution order to reveal the execution order of stream

---
+++ src/qnguyen/demo/ExecutionOrder.java
@@ -0,0 +1,22 @@
+package qnguyen.demo;
+
+import java.util.stream.Stream;
+
+/**
+ * Created by nguyenquang on 6/23/17.
+ */
+public class ExecutionOrder {
+
+    public static void main(String[] args) {
+        Stream.of("a", "b", "c", "d")
+        .filter( str -> {
+            System.out.println("filter" + str);
+            return str.startsWith("a");
+        })
+        .map( str -> {
+            System.out.println("map: " + str);
+            return str.toUpperCase();
+        })
+        .forEach(System.out::println);
+    }
+}
